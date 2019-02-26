bdf2995d50294a54a9a98647e2819eb7d8fb4c44
unknown <nathaliecortz@hotmail.com>
2019-02-26 11:13:49 -0600

    Dejé el código listo para empezar los test

--- src/main.js
+++ src/main.js
@@ -1,57 +1,43 @@
-
-//manda llamar la función de generar pokemon random al momento de cargar la pantalla
-window.onload= function init (){
-    document.getElementById("pokemonOfTheDay").style.display="block";
-    dataLovers.findPokemonRandom(POKEMON.pokemon, random(POKEMON.pokemon));
-    document.getElementById("open-hide").addEventListener( 'click' , changeClass ); 
-  };
-
-  function changeClass()
-  {
-   document.getElementById("open-hide").classList.toggle('show');
-  }
-  
- 
-
-//Bloqueo de pantallas que van a ser llamadas con el Ménu
-document.getElementById("pokemonList").style.display="none";
-document.getElementById("pokemonTypes").style.display="none";
-document.getElementById("statisticsList").style.display="none";
-
-
-//Crea un array con los nombres de los pokemones
-function pokemonName (data){
-  let arrayPokemonName=[];
-  for (let i=0; i<data.length; i++){
-    let pokemon=data[i];
-    arrayPokemonName.push(pokemon.name);
-  }
-  console.log (arrayPokemonName);
-}
-
-//Función para generar random
-let random= function (data){
-   randomId= Math.floor(((Math.random()*data.length)+1));
-   return randomId;
+ //Función para generar random
+ let random= function (data){
+  let randomId= Math.floor(((Math.random()*data.length)+1));
+  return randomId;
 };
 
 
 
+//manda llamar la función de generar pokemon random al momento de cargar la pantalla
+window.onload= function init (){
+    document.getElementById("pokemonOfTheDay").style.display="block";
+    let pokemon= dataLovers.findPokemonRandom(POKEMON.pokemon, random(POKEMON.pokemon));
+    printFirstData(pokemon);  
+  };
+ 
+//Bloqueo de pantallas que van a ser llamadas con el Ménu
+document.getElementById("pokemonList").style.display="none";
+document.getElementById("pokemonTypes").style.display="none";
+document.getElementById("statisticsList").style.display="none";
+document.getElementById("filterType").style.display="none";
+
 //función del botón random que llama a generar un pokemón aleatorio
 function showPokemon(){
-dataLovers.findPokemonRandom(POKEMON.pokemon, random(POKEMON.pokemon));
+let pokemon=dataLovers.findPokemonRandom(POKEMON.pokemon, random(POKEMON.pokemon));
+printAll(pokemon);
 }
 document.getElementById("random").addEventListener("click", showPokemon);
 
+//const buttons =document.getElementById("search");
+//buttons.innerHTML= '<img src = "https://image.flaticon.com/icons/svg/164/164995.svg"/>´;
 //Función del boton de buscar
 function searchButton(){
-//const filter = document.getElementById("searchPokemon")
-dataLovers.findPokemon(POKEMON.pokemon, document.getElementById("searchPokemon").value);
+let pokemon=dataLovers.findPokemon(POKEMON.pokemon, document.getElementById("searchPokemon").value);
+printAll(pokemon);
 document.getElementById("searchPokemon").value = "";
 document.getElementById("pokemonOfTheDay").style.display="block";
 document.getElementById("pokemonList").style.display="none";
 document.getElementById("pokemonTypes").style.display="none";
 document.getElementById("statisticsList").style.display="none";
+console.log(document.getElementById("searchPokemon").value);
 }
 document.getElementById("search").addEventListener("click", searchButton);
 
@@ -80,6 +66,7 @@ document.getElementById("search").addEventListener("click", searchButton);
      }
  }
 }    
+
 function printAll(pokemon){
 dataLovers.findPokemonRandom(POKEMON.pokemon, random);
   printFirstData(pokemon);
@@ -103,10 +90,21 @@ function menuTypes(){
   document.getElementById("pokemonOfTheDay").style.display="none";
   document.getElementById("pokemonList").style.display="none";
   document.getElementById("statisticsList").style.display="none";
+  document.getElementById("filterType").style.display="block";
 }
 
 document.getElementById("types").addEventListener("click", menuTypes);
 
+function botonFilterType (){
+  let type = document.getElementById("pokemonTypes").value;
+  console.log (type);
+  let filterProperties= dataLovers.filterType (POKEMON.pokemon, type);
+  filterProperties.forEach(pokemon=>(table(filterProperties)));
+}
+
+document.getElementById("filterType").addEventListener("click", botonFilterType);
+
+/*
 document.getElementById("water").addEventListener("click", function(){dataLovers.filterType(POKEMON.pokemon, "Water");});
 document.getElementById("poison").addEventListener("click", function(){dataLovers.filterType(POKEMON.pokemon, "Poison");});
 document.getElementById("normal").addEventListener("click", function(){dataLovers.filterType(POKEMON.pokemon, "Normal");});
@@ -122,21 +120,27 @@ document.getElementById("bug").addEventListener("click", function(){dataLovers.f
 document.getElementById("rock").addEventListener("click", function(){dataLovers.filterType(POKEMON.pokemon, "Rock");});
 document.getElementById("dragon").addEventListener("click", function(){dataLovers.filterType(POKEMON.pokemon, "Dragon");});
 document.getElementById("ghost").addEventListener("click", function(){dataLovers.filterType(POKEMON.pokemon, "Ghost");});
-
+*/
 function menuList(){
   document.getElementById("results").innerHTML=" ";
   document.getElementById("pokemonList").style.display="block";
   document.getElementById("pokemonOfTheDay").style.display="none";
   document.getElementById("pokemonTypes").style.display="none";
   document.getElementById("statisticsList").style.display="none";
-  tabla(POKEMON.pokemon);
+  document.getElementById("filterType").style.display="none";
+  table(POKEMON.pokemon);
   
 }
 document.getElementById("list").addEventListener("click", menuList);
 
-document.getElementById("filterSearch").addEventListener("click", function(){dataLovers.sortData(POKEMON.pokemon, document.getElementById("listFor").value, document.getElementById("listOrder").value);});
+function filterSearch (){
+  let pokemonOrder= dataLovers.sortData(POKEMON.pokemon, document.getElementById("listFor").value, document.getElementById("listOrder").value);
+  pokemonOrder.forEach(pokemon=>(table(pokemonOrder)));  
+}
 
-function tabla(data){
+document.getElementById("filterSearch").addEventListener("click", filterSearch);
+
+function table(data){
   const paragraph= document.getElementById("results");
   paragraph.innerHTML=" ";
     for (let i=0; i<data.length; i++){
@@ -167,6 +171,7 @@ function statistics(){
   document.getElementById("pokemonTypes").style.display="none";
   document.getElementById("statisticsList").style.display="none";
   document.getElementById("statisticsList").style.display="block";
+  document.getElementById("filterType").style.display="none";
 }
 document.getElementById("statistics").addEventListener("click", statistics);
 document.getElementById("statisticSearch").addEventListener("click", function(){dataLovers.computeStats(POKEMON.pokemon, document.getElementById("statisticsOptions").value);});
